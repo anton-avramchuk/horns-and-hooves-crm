5e78050c98a25ebdeebdc6539c785c5179c210c5
aavramchuk <aav1984@gmail.com>
2025-03-31 09:05:19 +0300

    add core libs

---
+++ src/frontend/crm/libs/core/permissions/src/lib/permissions/permissions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-permissions',
+  imports: [CommonModule],
+  templateUrl: './permissions.component.html',
+  styleUrl: './permissions.component.css',
+})
+export class PermissionsComponent {}
